3966e4571b6d8dcb1834388c7416f6d82fd9c358
sarp_03 <sarpereroglu2@gmail.com>
2026-04-07 15:34:10 +0200

    fix: dataprotection keys are stored in path and CefGlue binaries are explicitly exported when publishing

--- App.axaml.cs
+++ App.axaml.cs
@@ -21,10 +21,11 @@ public partial class App : Application
         AvaloniaXamlLoader.Load(this);
         Instance = this;
         
-        var keysDirectory = new DirectoryInfo(
-            Path.Combine(AppContext.BaseDirectory, "dataprotection-keys")
-        );
+        string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string configDir = Path.Combine(appData, "Snippy");
 
+        var keysDirectory = new DirectoryInfo(Path.Combine(configDir, "dataprotection-keys"));
+        
         var provider = DataProtectionProvider.Create(
             keysDirectory,
             options => options.SetApplicationName("Snippy")
